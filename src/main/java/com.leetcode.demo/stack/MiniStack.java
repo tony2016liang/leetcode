cb32a3d739c3f371d4b61905f216dd71745737ba
tony2016liang <tony2016liang@gmail.com>
2019-08-12 15:27:47 +0800

    question about stack

---
+++ src/main/java/com.leetcode.demo/stack/MiniStack.java
@@ -0,0 +1,58 @@
+package com.leetcode.demo.stack;
+
+/**
+ * 设计一个支持 push，pop，top 操作，并能在常数时间内检索到最小元素的栈。
+ *
+ * push(x) -- 将元素 x 推入栈中。
+ * pop() -- 删除栈顶的元素。
+ * top() -- 获取栈顶元素。
+ * getMin() -- 检索栈中的最小元素。
+ * 示例:
+ *
+ * MinStack minStack = new MinStack();
+ * minStack.push(-2);
+ * minStack.push(0);
+ * minStack.push(-3);
+ * minStack.getMin();   --> 返回 -3.
+ * minStack.pop();
+ * minStack.top();      --> 返回 0.
+ * minStack.getMin();   --> 返回 -2.
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/min-stack
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class MiniStack {
+
+    /**
+     * Your MinStack object will be instantiated and called as such:
+     * MinStack obj = new MinStack();
+     * obj.push(x);
+     * obj.pop();
+     * int param_3 = obj.top();
+     * int param_4 = obj.getMin();
+     */
+
+    /** initialize your data structure here. */
+    public MiniStack() {
+
+    }
+
+    public void push(int x) {
+
+    }
+
+    public void pop() {
+
+    }
+
+    public int top() {
+        //TODO
+        return 0;
+    }
+
+    public int getMin() {
+        //TODO
+        return 0;
+    }
+}
